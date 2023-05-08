c474b6eebf6431c9f49393b7e4cca6cdcce77712
Kris Tung <ragdoll.washoku89@gmail.com>
2023-05-08 14:49:31 +0800

    deploy

---
+++ assets/AdminProducts-d52dfbb6.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as o}from"./index-bfb67b23.js";const r={data(){return{}},methods:{},mounted(){}};function c(n,s,a,d,u,_){return e(),o("div",null," Products ")}const m=t(r,[["render",c]]);export{m as default};
